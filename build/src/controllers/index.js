355579fbc439bc30af7eb529999c844478df7a2e
Talento90 <marcotalento90@gmail.com>
2016-01-23 20:46:42 +0000

    ignore build folder

--- build/src/controllers/index.js
+++
@@ -1,9 +0,0 @@
-var TodoController_1 = require("./TodoController");
-function default_1(server) {
-    TodoController_1.default(server);
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = default_1;
-;
-
-//# sourceMappingURL=index.js.map
